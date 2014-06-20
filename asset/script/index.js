b2522fff9b4856473cff6a6e697ab1efaad101c3
denezra <den.segunial@gmail.com>
2014-06-19 18:49:35 -0700

    Added Scrollbar, Added js files, and modify codes

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,12 +1,17 @@
 //Jquery Instantie
 jQuery(function($) {
 	//Global variable
-	var extension = '.html';
-	var path      = '/asset/html/';
+	var files = {
+		extension	: '.html',
+		pageFolder	: 'page/',
+		path			: '/asset/html/'
+	};
 	//Window load
 	$(window).load(function(){
 		//Header div load
-		$('section.headerSection>div').load(path + 'header' + extension);
+		$('section.headerSection > div').load(files.path + 'header' + files.extension);
+		//Content div load
+		$('section.contentSection > div').load(files.path + files.pageFolder + 'home' + files.extension);
 	});
 
 });
